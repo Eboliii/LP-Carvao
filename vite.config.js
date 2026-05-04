9896db3080db56fd1b34ffa34ab067aaa24b454c
Eboliii <r.eboli.ben@gmail.com>
2026-05-04 18:48:47 -0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    allowedHosts: [
+      'node265865-env-7130093.sp1.br.saveincloud.net.br',
+      'env-7130093.sp1.br.saveincloud.net.br',
+    ],
+    hmr: {
+      clientPort: 80 
+    }
+  }
 })
